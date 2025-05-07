a7cc75906e9c954ddd194066232dd9d951549c85
ulastanersoyak <ulassztanersoyak@gmail.com>
2025-05-07 15:24:51 +0300

    expected changes for embedded systems

--- includes/cutils/expected.h
+++ includes/cutils/expected.h
@@ -1,94 +1,146 @@
 #ifndef CUTILS_EXPECTED_H
 #define CUTILS_EXPECTED_H
 
+#include "cutils/allocator.h"
+#include "cutils/config.h"
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
 typedef enum
 {
-  EXPECTED_OK,
-  EXPECTED_NULL_PTR,
-  EXPECTED_NO_MEMORY,
-  EXPECTED_INVALID_VALUE,
-  EXPECTED_INVALID_ARG
+  EXPECTED_OK = 0,
+  EXPECTED_NULL_PTR = 1,
+  EXPECTED_NO_MEMORY = 2,
+  EXPECTED_INVALID_ARG = 3,
+  EXPECTED_TIMEOUT = 4,
+  EXPECTED_ERROR = 5
 } expected_result_t;
 
 typedef struct
 {
-  union
-  {
-    void *value;
-    void *error;
-  } data;
-  bool has_value; // true = value, false = error
+  void *data;
+  size_t size;
+  expected_result_t error;
+  cutils_allocator_t *allocator;
 } expected_t;
+
+/**
+ * Creates a new expected value with the specified allocator.
+ *
+ * @param size Size of the value in bytes
+ * @param allocator Allocator to use
+ * @return Newly allocated expected value or NULL on error
+ */
+expected_t *expected_create_with_allocator (size_t size,
+                                            cutils_allocator_t *allocator);
+
+/**
+ * Creates a new expected value using the default allocator.
+ *
+ * @param size Size of the value in bytes
+ * @return Newly allocated expected value or NULL on error
+ */
+expected_t *expected_create (size_t size);
+
+/**
+ * Creates a new expected value from data with the specified allocator.
+ *
+ * @param data Data to copy
+ * @param size Size of the data in bytes
+ * @param allocator Allocator to use
+ * @return Newly allocated expected value or NULL on error
+ */
+expected_t *expected_from_data_with_allocator (const void *data, size_t size,
+                                               cutils_allocator_t *allocator);
+
+/**
+ * Creates a new expected value from data using the default allocator.
+ *
+ * @param data Data to copy
+ * @param size Size of the data in bytes
+ * @return Newly allocated expected value or NULL on error
+ */
+expected_t *expected_from_data (const void *data, size_t size);
+
+/**
+ * Creates a new expected error with the specified allocator.
+ *
+ * @param error Error code
+ * @param allocator Allocator to use
+ * @return Newly allocated expected error or NULL on error
+ */
+expected_t *expected_from_error_with_allocator (expected_result_t error,
+                                                cutils_allocator_t *allocator);
+
+/**
+ * Creates a new expected error using the default allocator.
+ *
+ * @param error Error code
+ * @return Newly allocated expected error or NULL on error
+ */
+expected_t *expected_from_error (expected_result_t error);
+
+/**
+ * Destroys expected value and frees all allocated memory.
+ *
+ * @param expected Expected value to destroy
+ */
+void expected_destroy (expected_t *expected);
+
+/**
+ * Gets the data from the expected value.
+ *
+ * @param expected Expected value to get data from
+ * @return Pointer to data or NULL if error
+ */
+void *expected_get_data (const expected_t *expected);
+
+/**
+ * Gets the error from the expected value.
+ *
+ * @param expected Expected value to get error from
+ * @return Error code
+ */
+expected_result_t expected_get_error (const expected_t *expected);
+
+/**
+ * Checks if the expected value has a value.
+ *
+ * @param expected Expected value to check
+ * @return true if has value, false otherwise
+ */
+bool expected_has_value (const expected_t *expected);
+
+/**
+ * Checks if the expected value has an error.
+ *
+ * @param expected Expected value to check
+ * @return true if has error, false otherwise
+ */
+bool expected_has_error (const expected_t *expected);
+
+/**
+ * Gets the size of the expected value.
+ *
+ * @param expected Expected value to get size from
+ * @return Size in bytes
+ */
+size_t expected_size (const expected_t *expected);
+
+/**
+ * Gets the memory usage of the expected value.
+ *
+ * @param expected Expected value to get memory usage from
+ * @return Memory usage in bytes
+ */
+size_t expected_memory_usage (const expected_t *expected);
 
 /**
  * Gets the last expected operation error.
  *
  * @return Last error code
  */
-[[nodiscard]] expected_result_t expected_get_error (void);
-
-/**
- * Creates a new expected with a success value.
- *
- * @param value Pointer to the value to copy
- * @param size Size of the value in bytes
- * @return Newly allocated expected or NULL on error
- * @note Sets error to EXPECTED_NULL_PTR if value is NULL
- * @note Sets error to EXPECTED_NO_MEMORY if memory allocation fails
- */
-[[nodiscard]] expected_t *expected_success (const void *value, size_t size);
-
-/**
- * Creates a new expected with an error value.
- *
- * @param error Pointer to the error value to copy
- * @param size Size of the error value in bytes
- * @return Newly allocated expected or NULL on error
- * @note Sets error to EXPECTED_NULL_PTR if error is NULL
- * @note Sets error to EXPECTED_NO_MEMORY if memory allocation fails
- */
-[[nodiscard]] expected_t *expected_error (const void *error, size_t size);
-
-/**
- * Checks if expected contains a success value.
- *
- * @param exp Expected to check
- * @return true if contains success value, false if contains error
- * @note Sets error to EXPECTED_NULL_PTR if exp is NULL
- */
-bool expected_has_value (const expected_t *exp);
-
-/**
- * Gets the success value if present.
- *
- * @param exp Expected to get value from
- * @return Pointer to success value or NULL if contains error
- * @note Sets error to EXPECTED_NULL_PTR if exp is NULL
- * @note Sets error to EXPECTED_INVALID_VALUE if contains error instead of
- * value
- */
-[[nodiscard]] void *expected_value (const expected_t *exp);
-
-/**
- * Gets the error value if present.
- *
- * @param exp Expected to get error from
- * @return Pointer to error value or NULL if contains success
- * @note Sets error to EXPECTED_NULL_PTR if exp is NULL
- * @note Sets error to EXPECTED_INVALID_VALUE if contains value instead of
- * error
- */
-[[nodiscard]] void *expected_error_value (const expected_t *exp);
-
-/**
- * Frees all memory associated with an expected.
- *
- * @param exp Expected to destroy
- * @note Does not free the contained value or error pointers
- */
-void expected_destroy (expected_t *exp);
+expected_result_t expected_get_last_error (void);
 
 #endif // CUTILS_EXPECTED_H
--- src/expected.c
+++ src/expected.c
@@ -1,151 +1,234 @@
 #include "cutils/expected.h"
-
-#include <stdlib.h>
+#include "cutils/config.h"
+#include <stdint.h>
 #include <string.h>
+#include <threads.h>
 
-static expected_result_t g_last_error = EXPECTED_OK;
+static thread_local expected_result_t g_last_error = EXPECTED_OK;
 
-[[nodiscard]] expected_result_t
-expected_get_error (void)
+[[maybe_unused]] static bool
+check_timeout ([[maybe_unused]] uint32_t start_time_ms,
+               [[maybe_unused]] uint32_t timeout_ms)
 {
-  return g_last_error;
+#if CUTILS_PLATFORM_BARE_METAL
+  // Implement platform-specific time check
+  return true;
+#else
+  uint32_t current_time = get_current_time_ms ();
+  return (current_time - start_time_ms) <= timeout_ms;
+#endif
 }
 
-[[nodiscard]] expected_t *
-expected_success (const void *value, size_t size)
+expected_t *
+expected_create_with_allocator (size_t size, cutils_allocator_t *allocator)
 {
   g_last_error = EXPECTED_OK;
 
-  if (value == NULL)
-    {
-      g_last_error = EXPECTED_NULL_PTR;
-      return NULL;
-    }
-
-  if (size == 0)
+  if (allocator == NULL)
     {
       g_last_error = EXPECTED_INVALID_ARG;
       return NULL;
     }
 
-  expected_t *success = (expected_t *)malloc (sizeof (expected_t));
-  if (success == NULL)
+  expected_t *expected = cutils_allocate_aligned (
+      allocator, sizeof (expected_t), CUTILS_ALIGNMENT);
+  if (expected == NULL)
     {
       g_last_error = EXPECTED_NO_MEMORY;
       return NULL;
     }
 
-  success->data.value = malloc (size);
-  if (success->data.value == NULL)
+  if (size > 0)
     {
-      free (success);
-      g_last_error = EXPECTED_NO_MEMORY;
-      return NULL;
+      expected->data
+          = cutils_allocate_aligned (allocator, size, CUTILS_ALIGNMENT);
+      if (expected->data == NULL)
+        {
+          cutils_deallocate (allocator, expected);
+          g_last_error = EXPECTED_NO_MEMORY;
+          return NULL;
+        }
+    }
+  else
+    {
+      expected->data = NULL;
     }
 
-  memcpy (success->data.value, value, size);
-  success->has_value = true;
-  return success;
+  expected->size = size;
+  expected->error = EXPECTED_OK;
+  expected->allocator = allocator;
+
+  return expected;
 }
 
-[[nodiscard]] expected_t *
-expected_error (const void *error, size_t size)
+expected_t *
+expected_create (size_t size)
+{
+  cutils_allocator_t default_allocator = cutils_create_default_allocator ();
+  return expected_create_with_allocator (size, &default_allocator);
+}
+
+expected_t *
+expected_from_data_with_allocator (const void *data, size_t size,
+                                   cutils_allocator_t *allocator)
 {
   g_last_error = EXPECTED_OK;
 
-  if (error == NULL)
-    {
-      g_last_error = EXPECTED_NULL_PTR;
-      return NULL;
-    }
-
-  if (size == 0)
+  if (data == NULL || allocator == NULL)
     {
       g_last_error = EXPECTED_INVALID_ARG;
       return NULL;
     }
 
-  expected_t *failure = (expected_t *)malloc (sizeof (expected_t));
-  if (failure == NULL)
+  expected_t *expected = expected_create_with_allocator (size, allocator);
+  if (expected == NULL)
     {
-      g_last_error = EXPECTED_NO_MEMORY;
       return NULL;
     }
 
-  failure->data.error = malloc (size);
-  if (failure->data.error == NULL)
+  memcpy (expected->data, data, size);
+  expected->error = EXPECTED_OK;
+
+  return expected;
+}
+
+expected_t *
+expected_from_data (const void *data, size_t size)
+{
+  cutils_allocator_t default_allocator = cutils_create_default_allocator ();
+  return expected_from_data_with_allocator (data, size, &default_allocator);
+}
+
+expected_t *
+expected_from_error_with_allocator (expected_result_t error,
+                                    cutils_allocator_t *allocator)
+{
+  g_last_error = EXPECTED_OK;
+
+  if (allocator == NULL)
     {
-      free (failure);
-      g_last_error = EXPECTED_NO_MEMORY;
+      g_last_error = EXPECTED_INVALID_ARG;
       return NULL;
     }
 
-  memcpy (failure->data.error, error, size);
-  failure->has_value = false;
-  return failure;
+  expected_t *expected = expected_create_with_allocator (0, allocator);
+  if (expected == NULL)
+    {
+      return NULL;
+    }
+
+  expected->error = error;
+
+  return expected;
+}
+
+expected_t *
+expected_from_error (expected_result_t error)
+{
+  cutils_allocator_t default_allocator = cutils_create_default_allocator ();
+  return expected_from_error_with_allocator (error, &default_allocator);
+}
+
+void
+expected_destroy (expected_t *expected)
+{
+  g_last_error = EXPECTED_OK;
+
+  if (expected == NULL)
+    {
+      g_last_error = EXPECTED_NULL_PTR;
+      return;
+    }
+
+  if (expected->data != NULL)
+    {
+      cutils_deallocate (expected->allocator, expected->data);
+    }
+  cutils_deallocate (expected->allocator, expected);
+}
+
+void *
+expected_get_data (const expected_t *expected)
+{
+  if (expected == NULL)
+    {
+      g_last_error = EXPECTED_NULL_PTR;
+      return NULL;
+    }
+
+  if (expected->error != EXPECTED_OK)
+    {
+      g_last_error = expected->error;
+      return NULL;
+    }
+
+  return expected->data;
+}
+
+expected_result_t
+expected_get_error (const expected_t *expected)
+{
+  if (expected == NULL)
+    {
+      g_last_error = EXPECTED_NULL_PTR;
+      return EXPECTED_NULL_PTR;
+    }
+
+  return expected->error;
 }
 
 bool
-expected_has_value (const expected_t *exp)
+expected_has_value (const expected_t *expected)
 {
   g_last_error = EXPECTED_OK;
 
-  if (exp == NULL)
+  if (expected == NULL)
     {
       g_last_error = EXPECTED_NULL_PTR;
       return false;
     }
-  return exp->has_value;
+
+  return expected->error == EXPECTED_OK;
 }
 
-[[nodiscard]] void *
-expected_value (const expected_t *exp)
+bool
+expected_has_error (const expected_t *expected)
 {
   g_last_error = EXPECTED_OK;
 
-  if (exp == NULL)
+  if (expected == NULL)
     {
       g_last_error = EXPECTED_NULL_PTR;
-      return NULL;
+      return true;
     }
 
-  if (exp->has_value == false)
-    {
-      g_last_error = EXPECTED_INVALID_VALUE;
-      return NULL;
-    }
-
-  return exp->data.value;
+  return expected->error != EXPECTED_OK;
 }
 
-[[nodiscard]] void *
-expected_error_value (const expected_t *exp)
+size_t
+expected_size (const expected_t *expected)
 {
-  g_last_error = EXPECTED_OK;
-
-  if (exp == NULL)
+  if (expected == NULL)
     {
       g_last_error = EXPECTED_NULL_PTR;
-      return NULL;
+      return 0;
     }
-
-  if (exp->has_value == true)
-    {
-      g_last_error = EXPECTED_INVALID_VALUE;
-      return NULL;
-    }
-
-  return exp->data.error;
+  return expected->size;
 }
 
-void
-expected_destroy (expected_t *exp)
+size_t
+expected_memory_usage (const expected_t *expected)
 {
-  if (exp == NULL)
+  if (expected == NULL)
     {
-      return;
+      g_last_error = EXPECTED_NULL_PTR;
+      return 0;
     }
+  return sizeof (expected_t) + expected->size;
+}
 
-  free (exp->data.value); // union means this frees either value or error
-  free (exp);
+expected_result_t
+expected_get_last_error (void)
+{
+  return g_last_error;
 }
